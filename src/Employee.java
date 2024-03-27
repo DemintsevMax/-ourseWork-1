95b3396e157a88e769812d95e7b9f41e083dffe0
Maksim <raymax.spb@gmail.com>
2024-03-27 18:16:36 +0300

    Курсовая работа 1

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Employee {
     private static int  counter = 1;
     private int id;
@@ -21,4 +23,58 @@ public class Employee {
         Employee.counter = counter;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
 }
