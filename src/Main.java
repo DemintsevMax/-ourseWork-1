291a12f265b32dd794ba160e94e9386667759866
Maksim <raymax.spb@gmail.com>
2024-03-27 18:28:23 +0300

    Курсовая работа 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
+        Employee[] employees = new Employee[10];
+        employees[0] = new Employee("Снежная Жанна", 1, 65_000);
+        employees[1] = new Employee("Смирнов Сергей", 3, 100_000);
+        employees[2] = new Employee("Иванов Иван", 2, 89_000);
+        employees[3] = new Employee("Молев Дима", 4, 56_000);
+        employees[4] = new Employee("Аглы Ибрагим", 1, 65_000);
+        employees[5] = new Employee("Снежков Алексей", 2, 77_000);
+        employees[6] = new Employee("Кабаева Алина", 4, 93_000);
+        employees[7] = new Employee("Медведев Дима", 2, 68_000);
+        employees[8] = new Employee("Пушкин Александр", 1, 98_000);
+        employees[9] = new Employee("Святая Вероника", 3, 112_000);
     }
 }
